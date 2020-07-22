1a5bbbf7c2395d3bd8b8ff3adc2c2cfb518c915c
Anton Benitez <benitezanton05@gmail.com>
2020-07-22 16:26:28 +0800

    boop

--- components/NavBar/navbar.jsx
+++ components/NavBar/navbar.jsx
@@ -3,7 +3,7 @@ import Hamburger from '../../public/assets/img/hamburger.svg'
 
 const NavBar = () => (
     <NavMobileWrapper>
-        <img src="../../public/assets/img/logo.svg" alt="CompSAt Logo"/>
+        <img src="img/logo.svg" alt="CompSAt Logo"/>
         <img src={Hamburger} alt="Hamburger Icon"/>
     </NavMobileWrapper>
 );
