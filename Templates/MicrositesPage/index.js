ec03c39f6cec51219d6bec76801a7fbc259c49cc
abbeeeeyyyyy <abigail.moreno@obf.ateneo.edu>
2020-08-13 13:26:33 +0800

    tried to fix things

--- Templates/MicrositesPage/index.js
+++ Templates/MicrositesPage/index.js
@@ -1,15 +1,14 @@
 import Microsite from '../../components/Microsite'
-import QPICalcImg from './microsite-qpi-calculator.svg'
+import QPICalcImg from '../../public/static/img/microsite-qpi-calculator.svg'
 import testimg from '../../public/static/img/microsite-qpi-calculator 2.svg'
 
 export default () => {
     return (
         <Microsite
             MicrositeTitle="QPI Calculator"
-            PrimaryIcon={QPICalcImg}
-            SecondaryIcon={testimg}
+           
             >   
         </Microsite>
         
     )
-}
+} 
--- components/Microsite/Microsite.js
+++ components/Microsite/Microsite.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { MicrositeContainer, IconBox, Title } from './styles'
+import QPICalcImg from '../../public/static/img/microsite-qpi-calculator.svg'
+import testimg from '../../public/static/img/microsite-qpi-calculator 2.svg'
 
 const Microsite = ({ MicrositeTitle, PrimaryIcon, SecondaryIcon }) => (
     <MicrositeContainer>
         <Title>{MicrositeTitle}</Title>
         <IconBox>
-            <img src={PrimaryIcon} alt="icon" />
+            {console.log(PrimaryIcon)}
+            <img src={QPICalcImg} alt="icon" />
+            
         </IconBox>
-        {SecondaryIcon}
-        <img src="/static/img/microsite-qpi-calculator 2.svg" alt="qpi-calc" style={{position: "absolute", top: "90px", left: "45px"}} /> 
+        <img src={testimg} alt="qpi-calc" style={{position: "absolute", top: "90px", left: "45px"}} /> 
     </MicrositeContainer>
 )
 
