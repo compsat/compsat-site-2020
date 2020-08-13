5dde3e34b749817b37826c4f99605c156a51ed9c
abbeeeeyyyyy <abigail.moreno@obf.ateneo.edu>
2020-08-13 14:51:50 +0800

    update

---
+++ Templates/MicrositesPage/components/Header/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { HeaderSpace } from './styles';
+
+const MicrositeHeader = () => (
+    <HeaderSpace>
+        <img src='/static/img/microsite-header.svg'></img>
+    </HeaderSpace>
+)
+
+export default MicrositeHeader;
---
+++ Templates/MicrositesPage/components/Header/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const HeaderSpace = styled.div`
+    position: relative;
+    top:-56px;
+    left: -24px;
+    
+`
