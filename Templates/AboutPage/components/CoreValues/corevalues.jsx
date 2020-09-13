aefba152d02c8c07ff26de2653d578159c74ad86
Joan Lee <55185980+joan-lee@users.noreply.github.com>
2020-09-13 22:48:52 +0800

    Fix alignment and margins of each core value

--- Templates/AboutPage/components/CoreValues/corevalues.jsx
+++ Templates/AboutPage/components/CoreValues/corevalues.jsx
@@ -1,38 +1,40 @@
+import { CoreValueLeft, CoreValueRight } from "./styles";
+
 const CoreValues = () => (
   <div>
     <h2>Our Core Values</h2>
     <div>
-      <div>
+      <CoreValueLeft>
         <img
           src="/static/img/corevalues_illustration1.svg"
           alt="Core Values Illustration: One Fourth of the CompSAt Logo"
         />
         <p>Continuously aspire for self-growth and excellence</p>
-      </div>
-      <div>
+      </CoreValueLeft>
+      <CoreValueRight>
         <img
           src="/static/img/corevalues_illustration2.svg"
           alt="Core Values Illustration: One Fourth of the CompSAt Log0"
         />
         <p>Leverage collaboration</p>
-      </div>
+      </CoreValueRight>
     </div>
     <div>
-      <div>
+      <CoreValueLeft>
         <img
           src="/static/img/corevalues_illustration3.svg"
           alt="Core Values Illustration: One Fourth of the CompSAt Logo"
         />
         <p>Cultivate an empowering, supportive, and caring community</p>
-      </div>
-      <div>
+      </CoreValueLeft>
+      <CoreValueRight>
         <img
           src="/static/img/corevalues_illustration4.svg"
           alt="Core Values Illustration: One Fourth of the CompSAt Logo"
         />
         <p>Provide impactful and innovative services</p>
-      </div>
-    </div>  
+      </CoreValueRight>
+    </div>
   </div>
 );
 
---
+++ Templates/AboutPage/components/CoreValues/styles.jsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const CoreValueLeft = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 0 32px 0;
+
+  img {
+    margin: 0 32px 16px 0;
+  }
+
+  p {
+    margin: 0;
+  }
+`;
+
+export const CoreValueRight = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 0 32px 0;
+
+  img {
+    margin: 0 0 16px 32px;
+  }
+
+  p {
+    margin: 0;
+  }
+`;
