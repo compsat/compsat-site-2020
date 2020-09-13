b5f94fff8982092234576129e34bb5cb569614e6
Joan Lee <55185980+joan-lee@users.noreply.github.com>
2020-09-14 02:01:57 +0800

    Differentiated Top2, EB, AC members with bg color, margins, text color

--- Templates/AboutPage/components/Officers/officers.jsx
+++ Templates/AboutPage/components/Officers/officers.jsx
@@ -1,225 +1,225 @@
-import { OfficerPicture, OfficerDetails } from "./styles";
+import { Top2Member, EBMember, ACMember, OfficerPicture, OfficerDetails } from "./styles";
 
 const Officers = () => (
   <div>
     <h2>Executive Board + Associates Council</h2>
     <div>
       <div>
-        <div>
+        <Top2Member>
           <OfficerPicture></OfficerPicture>
           <div>
             <p>Juan dela Cruz</p>
             <OfficerDetails>President</OfficerDetails>
             <OfficerDetails>first.last@compsat.org</OfficerDetails>
           </div>
-        </div>
-        <div>
+        </Top2Member>
+        <Top2Member>
           <OfficerPicture></OfficerPicture>
           <div>
             <p>Juan dela Cruz</p>
             <OfficerDetails>Vice President</OfficerDetails>
             <OfficerDetails>first.last@compsat.org</OfficerDetails>
           </div>
-        </div>
+        </Top2Member>
       </div>
 
       <div>
         <div>
           <h3>Department of Communications</h3>
-          <div>
+          <EBMember>
             <OfficerPicture></OfficerPicture>
             <div>
               <p>Juan dela Cruz</p>
               <OfficerDetails>Vice President for Communications</OfficerDetails>
               <OfficerDetails>first.last@compsat.org</OfficerDetails>
             </div>
-          </div>
-          <div>
+          </EBMember>
+          <ACMember>
             <OfficerPicture></OfficerPicture>
             <div>
               <p>Juan dela Cruz</p>
               <OfficerDetails>Associate Vice President for Promotions</OfficerDetails>
               <OfficerDetails>first.last@compsat.org</OfficerDetails>
             </div>
-          </div>
-          <div>
+          </ACMember>
+          <ACMember>
             <OfficerPicture></OfficerPicture>
             <div>
               <p>Juan dela Cruz</p>
               <OfficerDetails>Associate Vice President for Publications</OfficerDetails>
               <OfficerDetails>first.last@compsat.org</OfficerDetails>
             </div>
-          </div>
+          </ACMember>
         </div>
 
         <div>
           <h3>Department of Corporate Relations</h3>
-          <div>
+          <EBMember>
             <OfficerPicture></OfficerPicture>
             <div>
               <p>Juan dela Cruz</p>
               <OfficerDetails>Vice President for Corporate Relations</OfficerDetails>
               <OfficerDetails>first.last@compsat.org</OfficerDetails>
             </div>
-          </div>
-          <div>
+          </EBMember>
+          <ACMember>
             <OfficerPicture></OfficerPicture>
             <div>
               <p>Juan dela Cruz</p>
               <OfficerDetails>Associate Vice President for Corporate Relations</OfficerDetails>
               <OfficerDetails>first.last@compsat.org</OfficerDetails>
             </div>
-          </div>
+          </ACMember>
         </div>
 
         <div>
           <h3>Department of Development and Training</h3>
-          <div>
+          <EBMember>
             <OfficerPicture></OfficerPicture>
             <div>
               <p>Juan dela Cruz</p>
               <OfficerDetails>Vice President for Development and Training</OfficerDetails>
               <OfficerDetails>first.last@compsat.org</OfficerDetails>
             </div>
-          </div>
-          <div>
+          </EBMember>
+          <ACMember>
             <OfficerPicture></OfficerPicture>
             <div>
               <p>Juan dela Cruz</p>
               <OfficerDetails>Associate Vice President for Development Training</OfficerDetails>
               <OfficerDetails>first.last@compsat.org</OfficerDetails>
             </div>
-          </div>
-          <div>
+          </ACMember>
+          <ACMember>
             <OfficerPicture></OfficerPicture>
             <div>
               <p>Juan dela Cruz</p>
               <OfficerDetails>Associate Vice President for Programming Training</OfficerDetails>
               <OfficerDetails>first.last@compsat.org</OfficerDetails>
             </div>
-          </div>
-          <div>
+          </ACMember>
+          <ACMember>
             <OfficerPicture></OfficerPicture>
             <div>
               <p>Juan dela Cruz</p>
               <OfficerDetails>Associate Vice President for Development Services</OfficerDetails>
               <OfficerDetails>first.last@compsat.org</OfficerDetails>
             </div>
-          </div>
-          <div>
+          </ACMember>
+          <ACMember>
             <OfficerPicture></OfficerPicture>
             <div>
               <p>Juan dela Cruz</p>
               <OfficerDetails>Associate Vice President for Development Services</OfficerDetails>
               <OfficerDetails>first.last@compsat.org</OfficerDetails>
             </div>
-          </div>
+          </ACMember>
         </div>
       </div>
 
       <div>
         <div>
           <h3>Department of Documentation</h3>
-          <div>
+          <EBMember>
             <OfficerPicture></OfficerPicture>
             <div>
               <p>Juan dela Cruz</p>
               <p>Secretary-General</p>
               <p>first.last@compsat.org</p>
             </div>
-          </div>
-          <div>
+          </EBMember>
+          <ACMember>
             <OfficerPicture></OfficerPicture>
             <div>
               <p>Juan dela Cruz</p>
               <OfficerDetails>Associate Secretary-General</OfficerDetails>
               <OfficerDetails>first.last@compsat.org</OfficerDetails>
             </div>
-          </div>
+          </ACMember>
         </div>
 
         <div>
           <h3>Department of External Affairs</h3>
-          <div>
+          <EBMember>
             <OfficerPicture></OfficerPicture>
             <div>
               <p>Juan dela Cruz</p>
               <OfficerDetails>Vice President for External Affairs</OfficerDetails>
               <OfficerDetails>first.last@compsat.org</OfficerDetails>
             </div>
-          </div>
-          <div>
+          </EBMember>
+          <ACMember>
             <OfficerPicture></OfficerPicture>
             <div>
               <p>Juan dela Cruz</p>
               <OfficerDetails>Associate Vice President for Community Relations</OfficerDetails>
               <OfficerDetails>first.last@compsat.org</OfficerDetails>
             </div>
-          </div>
-          <div>
+          </ACMember>
+          <ACMember>
             <OfficerPicture></OfficerPicture>
             <div>
               <p>Juan dela Cruz</p>
               <OfficerDetails>Associate Vice President for Univeristy Relations</OfficerDetails>
               <OfficerDetails>first.last@compsat.org</OfficerDetails>
             </div>
-          </div>
+          </ACMember>
         </div>
 
         <div>
           <h3>Department of Finance</h3>
-          <div>
+          <EBMember>
             <OfficerPicture></OfficerPicture>
             <div>
               <p>Juan dela Cruz</p>
               <OfficerDetails>Vice President for Finance</OfficerDetails>
               <OfficerDetails>first.last@compsat.org</OfficerDetails>
             </div>
-          </div>
-          <div>
+          </EBMember>
+          <ACMember>
             <OfficerPicture></OfficerPicture>
             <div>
               <p>Juan dela Cruz</p>
               <OfficerDetails>Associate Vice President for Finance</OfficerDetails>
               <OfficerDetails>first.last@compsat.org</OfficerDetails>
             </div>
-          </div>
+          </ACMember>
         </div>
 
         <div>
           <h3>Department of Member Relations</h3>
-          <div>
+          <EBMember>
             <OfficerPicture></OfficerPicture>
             <div>
               <p>Juan dela Cruz</p>
               <OfficerDetails>Vice President for Member Relations</OfficerDetails>
               <OfficerDetails>first.last@compsat.org</OfficerDetails>
             </div>
-          </div>
-          <div>
+          </EBMember>
+          <ACMember>
             <OfficerPicture></OfficerPicture>
             <div>
               <p>Juan dela Cruz</p>
               <OfficerDetails>Associate Vice President for Member Formation</OfficerDetails>
               <OfficerDetails>first.last@compsat.org</OfficerDetails>
             </div>
-          </div>
-          <div>
+          </ACMember>
+          <ACMember>
             <OfficerPicture></OfficerPicture>
             <div>
               <p>Juan dela Cruz</p>
               <OfficerDetails>Associate Vice President for Member Formation</OfficerDetails>
               <OfficerDetails>first.last@compsat.org</OfficerDetails>
             </div>
-          </div>
-          <div>
+          </ACMember>
+          <ACMember>
             <OfficerPicture></OfficerPicture>
             <div>
               <p>Juan dela Cruz</p>
               <OfficerDetails>Associate Vice President for Member Involvement</OfficerDetails>
               <OfficerDetails>first.last@compsat.org</OfficerDetails>
             </div>
-          </div>
+          </ACMember>
         </div>
       </div>
 
--- Templates/AboutPage/components/Officers/styles.jsx
+++ Templates/AboutPage/components/Officers/styles.jsx
@@ -1,10 +1,45 @@
 import styled from "styled-components";
 
+export const Top2Member = styled.div`
+  display: flex;
+  align-items: center;
+  background: rgba(255, 255, 255, 0.85);
+  padding: 16px;
+  border-radius: 3px;
+  margin-bottom: 24px;
+  
+  p {
+    color: #07106D;
+  }
+`;
+
+export const EBMember = styled.div`
+  display: flex;
+  align-items: center;
+  background: rgba(255, 255, 255, 0.85);
+  padding: 16px;
+  border-radius: 3px;
+  margin-bottom: 8px;
+
+  p {
+    color: #07106D;
+  }
+`;
+
+export const ACMember = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 16px;
+  margin-bottom: 8px;
+`;
+
 export const OfficerPicture = styled.div`
   width: 74px;
   height: 74px;
   border: 3px #FFF solid;
   border-radius: 50%;
+  background: #07106D;
+  margin-right: 8px;
 `;
 
 export const OfficerDetails = styled.p`
