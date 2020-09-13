31bd114a06f98d6710c453492ff40d4e7d43fbac
Joan Lee <55185980+joan-lee@users.noreply.github.com>
2020-09-13 18:46:41 +0800

    Add content to Core Values Component, Add component to page index

---
+++ Templates/AboutPage/components/CoreValues/corevalues.jsx
@@ -0,0 +1,39 @@
+const CoreValues = () => (
+  <div>
+    <h2>Our Core Values</h2>
+    <div>
+      <div>
+        <img
+          src="/static/img/corevalues_illustration1.svg"
+          alt="Core Values Illustration: One Fourth of the CompSAt Logo"
+        />
+        <p>Continuously aspire for self-growth and excellence</p>
+      </div>
+      <div>
+        <img
+          src="/static/img/corevalues_illustration2.svg"
+          alt="Core Values Illustration: One Fourth of the CompSAt Log0"
+        />
+        <p>Leverage collaboration</p>
+      </div>
+    </div>
+    <div>
+      <div>
+        <img
+          src="/static/img/corevalues_illustration3.svg"
+          alt="Core Values Illustration: One Fourth of the CompSAt Logo"
+        />
+        <p>Cultivate an empowering, supportive, and caring community</p>
+      </div>
+      <div>
+        <img
+          src="/static/img/corevalues_illustration4.svg"
+          alt="Core Values Illustration: One Fourth of the CompSAt Logo"
+        />
+        <p>Provide impactful and innovative services</p>
+      </div>
+    </div>  
+  </div>
+);
+
+export default CoreValues;
--- Templates/AboutPage/index.js
+++ Templates/AboutPage/index.js
@@ -1,11 +1,13 @@
 import HeaderVisionMission from "./components/HeaderVisionMission";
 import CoreCompetencies from "./components/CoreCompetencies";
+import CoreValues from "./components/CoreValues";
 
 export default () => {
   return (
     <div>
       <HeaderVisionMission />
       <CoreCompetencies />
+      <CoreValues />
     </div>
   );
 };
