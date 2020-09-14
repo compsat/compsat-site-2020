29869062aa905a1743869061cce7ac874e19cfdd
Joan Lee <55185980+joan-lee@users.noreply.github.com>
2020-09-15 06:17:42 +0800

    Fix gradient margin problem, fix padding

--- Templates/AboutPage/styles.js
+++ Templates/AboutPage/styles.js
@@ -2,4 +2,6 @@ import styled from "styled-components";
 
 export const AboutPageWrapper = styled.div`
     background-image: linear-gradient(rgba(186,28,81,0) 0%, rgba(186,28,81,0.29) 18%, rgba(186,28,81,1) 30%, rgba(186,28,81,0.29) 48%, rgba(186,28,81,0) 50%);
+    margin: 0 -24px;
+    padding: 24px;
 `;
