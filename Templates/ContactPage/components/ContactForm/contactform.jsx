9b172829c14cdbb4c19806f6feb399f9f86efacc
Joan Lee <55185980+joan-lee@users.noreply.github.com>
2020-09-15 05:33:43 +0800

    Add error message, add styling, adjust form wrapper to changes

--- Templates/ContactPage/components/ContactForm/contactform.jsx
+++ Templates/ContactPage/components/ContactForm/contactform.jsx
@@ -1,48 +1,57 @@
-import {  ContactFormWrapper, FormField, WarningMessage, ButtonRight } from "./styles";
+import {  ContactFormWrapper, ErrorMessage, FormContent, FormField, WarningMessage, ButtonRight } from "./styles";
 import PrimaryButton from "../../../../components/PrimaryButton";
 
 const ContactForm = () => (
   <ContactFormWrapper>
-    <h3>Send us a message!</h3>
-    <form>
-      <FormField>
-        Full Name*
-        <input type="text" name="name" />
-        <WarningMessage>
-          <img
-            src="/static/img/warning_red.svg"
-            alt="Warning Symbol"
-          />
-          <p>Name is required.</p>
-        </WarningMessage>
-      </FormField>
+    <ErrorMessage>
+      <img
+        src="/static/img/warning_white.svg"
+        alt="Warning Symbol"
+      />
+      <p>Something went wrong. Please try again.</p>
+    </ErrorMessage>
+    <FormContent>
+      <h3>Send us a message!</h3>
+      <form>
+        <FormField>
+          Full Name*
+          <input type="text" name="name" />
+          <WarningMessage>
+            <img
+              src="/static/img/warning_red.svg"
+              alt="Warning Symbol"
+            />
+            <p>Name is required.</p>
+          </WarningMessage>
+        </FormField>
 
-      <FormField>
-        Email*
-        <input type="text" name="email" />
-        <WarningMessage>
-          <img
-            src="/static/img/warning_red.svg"
-            alt="Warning Symbol"
-          />
-          <p>Email format is incorrect.</p>
-        </WarningMessage>
-      </FormField>
+        <FormField>
+          Email*
+          <input type="text" name="email" />
+          <WarningMessage>
+            <img
+              src="/static/img/warning_red.svg"
+              alt="Warning Symbol"
+            />
+            <p>Email format is incorrect.</p>
+          </WarningMessage>
+        </FormField>
 
-      <FormField>
-        Message*
-        <textarea type="text" name="message" />
-        <WarningMessage>
-          <img
-            src="/static/img/warning_red.svg"
-            alt="Warning Symbol"
-          />
-          <p>Message is required.</p>
-        </WarningMessage>
-      </FormField>
+        <FormField>
+          Message*
+          <textarea type="text" name="message" />
+          <WarningMessage>
+            <img
+              src="/static/img/warning_red.svg"
+              alt="Warning Symbol"
+            />
+            <p>Message is required.</p>
+          </WarningMessage>
+        </FormField>
 
-      <ButtonRight><PrimaryButton>Submit</PrimaryButton></ButtonRight>
-    </form>
+        <ButtonRight><PrimaryButton>Submit</PrimaryButton></ButtonRight>
+      </form>
+    </FormContent>
   </ContactFormWrapper>
 );
 
--- Templates/ContactPage/components/ContactForm/styles.jsx
+++ Templates/ContactPage/components/ContactForm/styles.jsx
@@ -2,8 +2,24 @@ import styled from "styled-components";
 
 export const ContactFormWrapper = styled.div`
   background: #F6F6F6;
-  padding: 10%;
   border-radius: 10px;
+`;
+
+export const ErrorMessage = styled.div`
+  background: #BA1C51;
+  border-radius: 10px 10px 0 0;
+  display: flex;
+  align-items: center;
+  padding: 16px 10%;
+
+  p {
+    margin: 0 0 0 8px;
+    text-align: left;
+  }
+`;
+
+export const FormContent = styled.div`
+  padding: 24px 10%;
 
   h3 {
     color: #07106D;
