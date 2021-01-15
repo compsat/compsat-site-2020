49291032f9ed84ab1391a41d21fa01c8e8933e76
abbeeeeyyyyy <abigail.moreno@obf.ateneo.edu>
2021-01-15 15:37:55 +0800

    placed header image

--- Templates/ClientSites/index.js
+++ Templates/ClientSites/index.js
@@ -1,7 +1,14 @@
-
+import header_img from "../../public/static/img/clientsites_header.svg"
+import {
+    Header_Img,
+} from "./styles"
 
 export default () => {
     return(
-        <div></div>
+        <div>
+            <Header_Img> 
+                <img src={header_img} />
+            </Header_Img>
+        </div>
     );
 };
--- Templates/ClientSites/styles.js
+++ Templates/ClientSites/styles.js
@@ -1,1 +1,6 @@
 import styled from "styled-components"
+
+export const Header_Img = styled.div`
+    margin-top: -56px;
+    margin-left: 60px;
+`
