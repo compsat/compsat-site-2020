197ce81cda5aefb77ad90e177b6d18ee1588518b
Joan Lee <55185980+joan-lee@users.noreply.github.com>
2021-01-17 00:11:41 +0800

    Fix Core Compentencies responsiveness

--- Templates/AboutPage/components/CoreCompetencies/corecompetencies.jsx
+++ Templates/AboutPage/components/CoreCompetencies/corecompetencies.jsx
@@ -1,22 +1,22 @@
-import { CoreCompWrapper, CoreCompBox } from "./styles";
+import { CoreCompWrapper, CoreCompBox1, CoreCompBox2 } from "./styles";
 
 const CoreCompetencies = () => (
   <CoreCompWrapper>
     <h2>Our Core Competencies</h2>
-    <CoreCompBox>
+    <CoreCompBox1>
       <img
         src="/static/img/corecompetencies_illustration1.svg"
         alt="Core Competency Illustration: Providing Programming and Software Development Training"
       />
       <p>We provide programming and software development training that caters to both the interests of the members and meets the demands of the industry through workshops, mentorship programs, and the like. </p>
-    </CoreCompBox>
-    <CoreCompBox>
+    </CoreCompBox1>
+    <CoreCompBox2>
       <img
         src="/static/img/corecompetencies_illustration2.svg"
         alt="Core Competency Illustration: Providing Programming and Software Development services"
       />
       <p>We provide sustainable, effective, and high-quality programming and software development services for students, as well as commercial and non-profit organizations based within and outside the Ateneo community.</p>
-    </CoreCompBox>
+    </CoreCompBox2>
   </CoreCompWrapper>
 );
 
