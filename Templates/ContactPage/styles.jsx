b6cb5105fef580dcc8cc3757e11a8f8276ec7a4b
Joan Lee <55185980+joan-lee@users.noreply.github.com>
2021-01-17 19:42:07 +0800

    Configure ContactPageWrapper flex row at min-width 1200px

--- Templates/ContactPage/styles.jsx
+++ Templates/ContactPage/styles.jsx
@@ -4,8 +4,15 @@ export const ContactPageWrapper = styled.div`
     background: linear-gradient(170deg, rgba(186,28,81,0) 0%, rgba(186,28,81,0.14) 20%, rgba(186,28,81,0.35) 35%, rgba(186,28,81,1) 55%, rgba(186,28,81,0.51) 100%);
     margin: 0 -128px;
     padding: 32px 128px 48px 128px;
+    display: flex;
+    flex-direction: column;
 
-    @media screen and (min-width: 700px) {
+    @media screen and (min-width: 800px) {
       padding: 72px 128px;
+    }
+
+    @media screen and (min-width: 1200px) {
+      flex-direction: row;
+      justify-content: space-between;
     }
 `;
