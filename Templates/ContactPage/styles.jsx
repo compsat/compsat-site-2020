a54b06755385bbb304df322370499b19c01746f0
Joan Lee <55185980+joan-lee@users.noreply.github.com>
2021-01-17 19:38:19 +0800

    Fix Contact page padding responsiveness

--- Templates/ContactPage/styles.jsx
+++ Templates/ContactPage/styles.jsx
@@ -2,7 +2,10 @@ import styled from "styled-components";
 
 export const ContactPageWrapper = styled.div`
     background: linear-gradient(170deg, rgba(186,28,81,0) 0%, rgba(186,28,81,0.14) 20%, rgba(186,28,81,0.35) 35%, rgba(186,28,81,1) 55%, rgba(186,28,81,0.51) 100%);
-    width: 100vw;
-    margin: 0 -24px;
-    padding: 32px 24px 48px 24px;
+    margin: 0 -128px;
+    padding: 32px 128px 48px 128px;
+
+    @media screen and (min-width: 700px) {
+      padding: 72px 128px;
+    }
 `;
