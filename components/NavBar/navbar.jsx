9f9272ac2844bfdf6e5a6802e0eb59895eb0a8e7
Emman Evangelista <emmanuel.evangelista@obf.ateneo.edu>
2021-09-03 16:22:56 +0800

    remove null container for mobile nav

--- components/NavBar/navbar.jsx
+++ components/NavBar/navbar.jsx
@@ -71,68 +71,65 @@ const NavBar = () => {
             />
           </svg>
         </Hamburger>
-        {
-          clicked ? <MobileMenuContainer>
-            <CrossButton onClick={handleClick}>
-              <svg
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M5.63599 5.63604L18.3639 18.364"
-                  stroke="#f6f6f6"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-                <path
-                  d="M5.63599 18.364L18.3639 5.63604"
-                  stroke="#f6f6f6"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            </CrossButton>
-            <PseudoButton onClick={handleClick}>
-              <Link href="/">
-                <a>Home</a>
-              </Link>
-            </PseudoButton>
-            <PseudoButton onClick={handleClick}>
-              <Link href="/about" onClick={() => setClick(false)}>
-                <a>About Us</a>
-              </Link>
-            </PseudoButton>
-            <PseudoButton onClick={handleClick}>
-              <Link href="/events" onClick={() => setClick(false)}>
-                <a>Events</a>
-              </Link>
-            </PseudoButton>
-            <PseudoButton onClick={handleClick}>
-              <Link href="/services" onClick={() => setClick(false)}>
-                <a>Services</a>
-              </Link>
-            </PseudoButton>
-            <PseudoButton onClick={handleClick}>
-              <Link href="/client-sites" onClick={() => setClick(false)}>
-                <a>Client Sites</a>
-              </Link>
-            </PseudoButton>
-            <PseudoButton onClick={handleClick}>
-              <Link href="/microsites" onClick={() => setClick(false)}>
-                <a>Microsites</a>
-              </Link>
-            </PseudoButton>
-            <PseudoButton onClick={handleClick}>
-              <Link href="/contact-us" onClick={() => setClick(false)}>
-                <a>Contact Us</a>
-              </Link>
-            </PseudoButton>
-          </MobileMenuContainer>
-          : null
-        }
+        <MobileMenuContainer clicked={clicked ? "flex" : "none"}>
+          <CrossButton onClick={handleClick}>
+            <svg
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M5.63599 5.63604L18.3639 18.364"
+                stroke="#f6f6f6"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <path
+                d="M5.63599 18.364L18.3639 5.63604"
+                stroke="#f6f6f6"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </CrossButton>
+          <PseudoButton onClick={handleClick}>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </PseudoButton>
+          <PseudoButton onClick={handleClick}>
+            <Link href="/about" onClick={() => setClick(false)}>
+              <a>About Us</a>
+            </Link>
+          </PseudoButton>
+          <PseudoButton onClick={handleClick}>
+            <Link href="/events" onClick={() => setClick(false)}>
+              <a>Events</a>
+            </Link>
+          </PseudoButton>
+          <PseudoButton onClick={handleClick}>
+            <Link href="/services" onClick={() => setClick(false)}>
+              <a>Services</a>
+            </Link>
+          </PseudoButton>
+          <PseudoButton onClick={handleClick}>
+            <Link href="/client-sites" onClick={() => setClick(false)}>
+              <a>Client Sites</a>
+            </Link>
+          </PseudoButton>
+          <PseudoButton onClick={handleClick}>
+            <Link href="/microsites" onClick={() => setClick(false)}>
+              <a>Microsites</a>
+            </Link>
+          </PseudoButton>
+          <PseudoButton onClick={handleClick}>
+            <Link href="/contact-us" onClick={() => setClick(false)}>
+              <a>Contact Us</a>
+            </Link>
+          </PseudoButton>
+        </MobileMenuContainer>
       </NavMobileWrapper>
       <NavDesktopWrapper>
         <Link href="/" passHref>
