8890fe99357049bed83ebe92f68d7c27e07418ab
Emman Evangelista <emmanuel.evangelista@obf.ateneo.edu>
2021-09-03 16:00:43 +0800

    responsiveness optimization for about page

--- Templates/AboutPage/components/HeaderVisionMission/styles.jsx
+++ Templates/AboutPage/components/HeaderVisionMission/styles.jsx
@@ -16,20 +16,30 @@ export const HVMWrapper = styled.div`
     z-index: 2;
     position: relative;
   }
+
+  @media screen and (min-width: 1600px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 `;
 
 export const MissionContainer = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
+
   @media screen and (min-width: 800px) {
     flex-direction: row;
     justify-content: space-between;
+    align-items: stretch;
+
+    max-width: 1280px;
+    margin: 0 auto;
   }
 `;
 
-export const MissionText = styled.p`
-  width: 100%;
-`;
+export const MissionText = styled.p``;
 
 export const MissionBox = styled.div`
   padding: 15% 10%;
@@ -38,6 +48,8 @@ export const MissionBox = styled.div`
   background: #07106D;
   background-image: linear-gradient(rgba(186,28,81,0.28) 10%, rgba(186,28,81,1) 70%, rgba(186,28,81,0.6) 100%);
   margin-bottom: 32px;
+  max-width: 320px;
+  width: 100%;
 
   p {
     margin: 16px 0 0 0;
@@ -47,5 +59,6 @@ export const MissionBox = styled.div`
   @media screen and (min-width: 800px) {
     width: 32%;
     padding: 7% 3%;
+    max-width: 512px;
   }
 `;
